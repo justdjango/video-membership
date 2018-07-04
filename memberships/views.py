534d31c2eda640e8270e9aafaa2997a5cddf0207
mattfreire <freire1996@gmail.com>
2018-07-04 17:47:15 +0200

    membership select view part 3

--- memberships/views.py
+++ memberships/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 
 from django.views.generic import ListView
 
-from .models import Membership
+from .models import Membership, UserMembership
 
+
+def get_user_membership(request):
+	user_membership_qs = UserMembership.objects.filter(user=request.user)
+	if user_membership_qs.exists():
+		return user_membership_qs.first()
+	return None
 
 
 class MembershipSelectView(ListView):
 	model = Membership
 
-	
+	def get_context_data(self, *args, **kwargs):
+		context = super().get_context_data(**kwargs)
+		current_membership = get_user_membership(self.request)
+		context['current_membership'] = str(current_membership.membership)
+		return context
