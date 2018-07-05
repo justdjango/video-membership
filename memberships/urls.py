0fbfa642cfe9ef2653b0ab4759f6623f9f512176
mattfreire <freire1996@gmail.com>
2018-07-06 01:24:19 +0200

    Profile view added

--- memberships/urls.py
+++ memberships/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import MembershipSelectView, PaymentView
+from .views import (
+	MembershipSelectView, 
+	PaymentView, 
+	updateTransactionRecords,
+	profile_view)
 
 app_name = 'memberships'
 
 urlpatterns = [
     path('', MembershipSelectView.as_view(), name='select'),
-    path('payment', PaymentView, name='payment')
+    path('payment/', PaymentView, name='payment'),
+    path('update-transactions/<subscription_id>/', updateTransactionRecords, name='update-transactions'),
+    path('profile/', profile_view, name='profile')
 ]
--- memberships/views.py
+++ memberships/views.py
@@ -1,13 +1,23 @@
 from django.conf import settings
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.urls import reverse
 
 from .models import Membership, UserMembership, Subscription
 
 import stripe
+
+
+def profile_view(request):
+	user_membership = get_user_membership(request)
+	user_subscription = get_user_subscription(request)
+	context = {
+		'user_membership': user_membership,
+		'user_subscription': user_subscription
+	}
+	return render(request, "memberships/profile.html", context)
 
 
 def get_user_membership(request):
@@ -43,17 +53,16 @@ class MembershipSelectView(ListView):
 		return context
 
 	def post(self, request, **kwargs):
-		selected_membership_type = request.POST.get('membership_type')
-
 		user_membership = get_user_membership(request)
 		user_subscription = get_user_subscription(request)
 
+		selected_membership_type = request.POST.get('membership_type')
+
 		selected_membership_qs = Membership.objects.filter(
 				membership_type=selected_membership_type)
-
-		if selected_membership_qs.exists():
-			selected_membership = selected_membership_qs.first()
-
+		print(selected_membership_qs)
+		selected_membership = selected_membership_qs.first()
+		print(selected_membership)
 		'''
 		==========
 		VALIDATION
@@ -84,7 +93,7 @@ def PaymentView(request):
 	if request.method == "POST":
 		try:
 			token = request.POST['stripeToken']
-			stripe.Subscription.create(
+			subscription = stripe.Subscription.create(
 			  customer=user_membership.stripe_customer_id,
 			  items=[
 			    {
@@ -93,6 +102,11 @@ def PaymentView(request):
 			  ],
 			  source=token # 4242424242424242
 			)
+
+			return redirect(reverse('memberships:update-transactions',
+				kwargs={
+					'subscription_id': subscription.id
+				}))
 
 		except stripe.CardError as e:
 			messages.info(request, "Your card has been declined")
@@ -105,6 +119,25 @@ def PaymentView(request):
 	return render(request, "memberships/membership_payment.html", context)
 
 
+def updateTransactionRecords(request, subscription_id):
+	user_membership = get_user_membership(request)
+	selected_membership = get_selected_membership(request)
+
+	user_membership.membership = selected_membership
+	user_membership.save()
+
+	sub, created = Subscription.objects.get_or_create(user_membership=user_membership)
+	sub.stripe_subscription_id = subscription_id
+	sub.active = True
+	sub.save()
+
+	try:
+		del request.session['selected_membership_type']
+	except:
+		pass
+
+	messages.info(request, 'Successfully created {} membership'.format(selected_membership))
+	return redirect('/memberships')
 
 
 
